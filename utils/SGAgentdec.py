071afbbac39dfc71108b2665a1a4b8c28e27254b
Akhi Mocherla <59666032+akhimoche@users.noreply.github.com>
2024-08-06 17:35:52 +0100

    updated hyperparameters
    and removed constant probability SAagent as decay stronger

--- utils/SGAgentdec.py
+++ utils/SGAgentdec.py
@@ -2,11 +2,14 @@ import numpy as np
 import random
 
 class GAgent:
-    def __init__(self, num_tasks, sval, start_task, exploration_rate=0.1):
+    def __init__(self, num_tasks, sval, start_task, initial_temperature=1.0, temperature_decay=0.92, min_temperature=0.1):
         # Player values
         self.threshold = sval
         self.current_task = start_task
-        self.exploration_rate = exploration_rate
+        self.temperature = initial_temperature
+        self.initial_temperature = initial_temperature
+        self.temperature_decay = temperature_decay
+        self.min_temperature = min_temperature
 
         # Player data storage
         self.sum_table = np.zeros((num_tasks))
@@ -14,12 +17,12 @@ class GAgent:
         self.satisfaction_table = np.zeros((num_tasks))  # assume all coalitions meet threshold initially so they explore
         self.num_tasks = num_tasks
 
-    def softmax(self, x):
-        e_x = np.exp(x - np.max(x))
+    def softmax(self, x, temperature):
+        e_x = np.exp((x - np.max(x)) / temperature)
         return e_x / e_x.sum()
 
     def choose_action_move(self):
-        """Choose a new action based on the satisfaction of neighboring tasks."""
+        """Choose a new action based on the satisfaction of neighboring tasks using Boltzmann exploration."""
 
         left_task = (self.current_task - 1) % self.num_tasks
         right_task = (self.current_task + 1) % self.num_tasks
@@ -28,18 +31,19 @@ class GAgent:
         left_satisf = self.satisfaction_table[left_task]
         right_satisf = self.satisfaction_table[right_task]
 
-        if np.random.rand() < self.exploration_rate:
-            # Explore with a small probability
-            mu = random.randint(1, 2)
-        else:
-            # Select the move that leads to the highest satisfaction
-            satisfactions = [current_satisf, left_satisf, right_satisf]
-            mu = np.argmax(satisfactions)
+        # Use Boltzmann exploration to probabilistically choose a move
+        satisfactions = np.array([current_satisf, left_satisf, right_satisf])
+        probabilities = self.softmax(satisfactions, self.temperature)
+
+        mu = np.random.choice([0, 1, 2], p=probabilities)
 
         if mu == 1:
             self.current_task = left_task
         elif mu == 2:
             self.current_task = right_task
+
+        # Decay the temperature
+        self.temperature = max(self.min_temperature, self.temperature * self.temperature_decay)
 
         return mu
 
